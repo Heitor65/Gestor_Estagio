2e299bb2450c9e971bcbd8cbe77166ddc8255fc5
Heitor65 <multheitor@gmail.com>
2026-07-12 20:07:55 -0300

    registro das entidades no django admin

--- gestor_estagio/admin.py
+++ gestor_estagio/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Usuario, Aluno, Secretaria, Coordenador, Empresa, Tce, RelatorioSemestral, Estagio
 
-# Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Aluno)
+admin.site.register(Secretaria)
+admin.site.register(Coordenador)
+admin.site.register(Empresa)
+admin.site.register(Tce)
+admin.site.register(RelatorioSemestral)
+admin.site.register(Estagio)
