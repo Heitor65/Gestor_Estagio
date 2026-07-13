7c67f45f5fe61e1c8628c4eca598d9d0d04a8c0f
Heitor65 <multheitor@gmail.com>
2026-07-13 20:24:56 -0300

    add: JWT

--- setup/settings.py
+++ setup/settings.py
@@ -44,8 +44,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'gestor_estagio',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
